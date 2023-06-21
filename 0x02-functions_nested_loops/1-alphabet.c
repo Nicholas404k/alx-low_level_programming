cc9c3c19a3d2773f1e7d842e3d627042e574c223
Nicholas404k <ballloner8@gmail.com>
2023-06-21 05:08:24 -0700

    1-alphabet

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+  *print_alphabet - A function that prints the alphabets in LCase
+  *
+  */
+
+void print_alphabet(void)
+{
+	int n;
+
+	for (n = 97; n <= 122; n++)
+	{
+		_putchar('n');
+	}
+	_putchar('\n');
+}
