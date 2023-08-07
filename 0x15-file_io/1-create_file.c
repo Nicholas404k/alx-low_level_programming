8400d4c64687f7623780a3f9e46a5c34bfdc090c
Nicholas404k <ballloner8@gmail.com>
2023-08-07 10:30:30 -0700

    create_file

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -9,29 +9,25 @@
 
 int create_file(const char *filename, char *text_content)
 {
-	int fd;
-	int bytes_written = 0;
+	int fd, written_bytes, len = 0;
 
 	if (filename == NULL)
 		return (-1);
 
-	fd = open(filename, O_WRONLY | O_TRUNC, 0600);
-
-	if (fd == -1)
-		return (-1);
-
 	if (text_content != NULL)
 	{
-		while (text_content[bytes_written] != '\0')
-			bytes_written++;
-		bytes_written = write(fd, text_content, bytes_written);
-		if (bytes_written == -1)
-		{
-			close(fd);
-			return (-1);
-		}
+		for (len = 0; text_content[len];)
+			len++;
 	}
 
+	fd = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
+	written_bytes = write(fd, text_content, len);
+
+	if (fd == -1 || written_bytes == -1)
+		return (-1);
+
 	close(fd);
+
 	return (1);
+
 }
