a4423a337c7b38fd6d79f49d70652766c9e14b20
Nicholas404k <ballloner8@gmail.com>
2023-08-12 08:43:12 -0700

    100-times_table

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -9,39 +9,42 @@
 
 void print_times_table(int n)
 {
-	int i;
-	int j;
-	int product;
+	int i, j, product;
 
-	if (n < 0 || n > 15)
-		return;
-
-	for (i = 0; i <= n; i++)
+	if (n >= 0 && n <= 15)
 	{
-		for (j = 0; j <= n; j++)
+		for (i = 0; i <= n; i++)
 		{
-			product = i * j;
-
-			if (product < 10)
+			for (j = 0; j <= n; j++)
 			{
-				_putchar(' ');
-				_putchar(' ');
-				_putchar('0' + product);
-			} else if (product < 100)
-			{
-				_putchar(' ');
-				_putchar('0' + product / 10);
-				_putchar('0' + product % 10);
-			} else
-			{
-				_putchar('0' + product / 100);
-				_putchar('0' + (product % 100) / 10);
-				_putchar('0' + product % 10);
+				product = j * i;
+				if (j == 0)
+				{
+					_putchar(product + '0');
+				} else if (product < 10 && j != 0)
+				{
+					_putchar(',');
+					_putchar(' ');
+					_putchar(' ');
+					_putchar(' ');
+					_putchar(product + '0');
+				} else if (product >= 10 && product < 100)
+				{
+					_putchar(',');
+					_putchar(' ');
+					_putchar(' ');
+					_putchar((product / 10) + '0');
+					_putchar((product % 10) + '0');
+				} else if (product >= 100)
+				{
+					_putchar(',');
+					_putchar(' ');
+					_putchar((product / 100) + '0');
+					_putchar(((product / 10) % 10) + '0');
+					_putchar((product % 10) + '0');
+				}
 			}
-			_putchar(' ');
+			_putchar('\n');
 		}
-
-		_putchar('\n');
 	}
 }
-
