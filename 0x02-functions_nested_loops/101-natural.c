da93e17442d1b0260d66dd7e65b9dbf9f5b96443
Nicholas404k <ballloner8@gmail.com>
2023-08-12 09:02:17 -0700

    101-natural

--- 0x02-functions_nested_loops/101-natural.c
+++ 0x02-functions_nested_loops/101-natural.c
@@ -1,15 +1,13 @@
 #include "main.h"
 #include <stdio.h>
 
-int calculate_sum(void);
-
 /**
-  *calculate_sum - function that calculates the sum of all
+  *main - function that calculates the sum of all
   *                the multiples of 3 or 5 below 1024
   *Return: always 0
   */
 
-int calculate_sum(void)
+int main(void)
 {
 	int multiples_sum = 0;
 	int num;
