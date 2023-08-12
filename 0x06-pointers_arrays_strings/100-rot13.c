bd8ff903cfe09ba38acc1dce6fe99b994e02f440
Nicholas404k <ballloner8@gmail.com>
2023-08-12 10:53:10 -0700

    100-rot13

---
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -0,0 +1,39 @@
+#include "main.h"
+#include <stdio.h>
+/**
+  *rot13 - function that encodes a string using rot13
+  *@str: string to encode
+  *Return: always 0
+  */
+
+char *rot13(char *str)
+{
+	char *ptr;
+	char c;
+
+	if (str == NULL)
+		return (NULL);
+
+	*ptr = str;
+
+	while (*ptr != '\0')
+	{
+		c = *ptr;
+
+		if ((c >= 'A' && c <= 'Z') || (c >= 'a' && c <= 'z'))
+		{
+			if ((c >= 'A' && c <= 'M') || (c >= 'a' && c <= 'm'))
+			{
+				c += 13;
+			} else
+			{
+				c -= 13;
+			}
+		}
+
+		*ptr = c;
+		ptr++;
+	}
+
+	return (str);
+}
