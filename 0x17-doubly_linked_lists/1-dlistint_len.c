3c1d0d891244ceab5aaf5bb389f92b0ec2d5858d
Nicholas404k <ballloner8@gmail.com>
2023-09-15 12:39:17 -0700

    0-print_dlistint 2-add_dnodeint 1-dlistint_len

--- 0x17-doubly_linked_lists/1-dlistint_len.c
+++ 0x17-doubly_linked_lists/1-dlistint_len.c
@@ -10,14 +10,21 @@
 
 size_t dlistint_len(const dlistint_t *h)
 {
-    size_t count = 0;
+	int count;
 
-    while (h != NULL)
-    {
-        count++;
-        h = h->next;
-    }
+	count = 0;
 
-    return count;
+	if (h == NULL)
+		return (count);
 
+	while (h->prev != NULL)
+		h = h->prev;
+
+	while (h != NULL)
+	{
+		count++;
+		h = h->next;
+	}
+
+	return (count);
 }
